ed21df32d046c06f6a511b2b02ec37af00305f68
Minor Gordon <minorg@server.fake>
2026-04-13 15:03:34 -0400

    transform IJPDS data

--- apps/cli/src/cli.ts
+++ apps/cli/src/cli.ts
@@ -1,8 +1,14 @@
 #!/usr/bin/env node
 
-import { command, run, subcommands } from "cmd-ts";
+import fs from "node:fs/promises";
+import { command, positional, run, subcommands } from "cmd-ts";
+import { ExistingPath } from "cmd-ts/dist/cjs/batteries/fs.js";
 // import { pino } from "pino";
 import { generateJsonSchema } from "./generateJsonSchema.js";
+import {
+  IjpdsDataset,
+  transformIjpdsDataset,
+} from "./transformIjpdsDataset.js";
 
 // const _logger = pino(
 //   {
@@ -22,15 +28,42 @@ run(
       generate: subcommands({
         cmds: {
           "json-schema": command({
-            name: "json-schema",
-            description: "generate JSON schema",
             args: {},
-            handler: async () => {
-              console.log(JSON.stringify(generateJsonSchema(), null, 2));
+            description: "generate JSON schema",
+            handler: () => {
+              process.stdout.write(
+                JSON.stringify(generateJsonSchema(), null, 2),
+              );
             },
+            name: "json-schema",
           }),
         },
         name: "generate",
+      }),
+      transform: subcommands({
+        cmds: {
+          "ijpds-dataset": command({
+            args: {
+              inputFilePath: positional({
+                displayName: "inputFilePath",
+                description: "path to IJPDS dataset .json file",
+                type: ExistingPath,
+              }),
+            },
+            handler: async ({ inputFilePath }) => {
+              for (const model of transformIjpdsDataset(
+                IjpdsDataset.parse(
+                  JSON.parse((await fs.readFile(inputFilePath)).toString()),
+                ),
+              )) {
+                process.stdout.write(JSON.stringify(model));
+                process.stdout.write("\n");
+              }
+            },
+            name: "ijpds-dataset",
+          }),
+        },
+        name: "transform",
       }),
     },
     description: "shaclmate command line interface",
