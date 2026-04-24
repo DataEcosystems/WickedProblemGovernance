d85ee6ac5b4e4e151fdaac7b2eca333da338fe54
Minor Gordon <minorg@server.fake>
2026-04-24 13:43:14 -0400

    turn ObjectMeta and PropertyMeta into classes, get rid of other helpers

--- packages/model/src/PropertyMeta.ts
+++ packages/model/src/PropertyMeta.ts
@@ -1,5 +1,34 @@
-export type PropertyMeta = {
+import { z } from "zod";
+import { ObjectMeta } from "./ObjectMeta.js";
+
+export class PropertyMeta {
   readonly description: string;
   readonly range?: string;
   readonly title: string;
-};
+
+  constructor({
+    description,
+    range,
+    title,
+  }: {
+    readonly description: string;
+    readonly range?: z.ZodType | string;
+    readonly title: string;
+  }) {
+    this.description = description;
+    this.title = title;
+
+    if (typeof range === "string") {
+      this.range = range;
+    } else if (range != null) {
+      const meta = range.meta() as ObjectMeta | undefined;
+      const id = meta?.id;
+      if (id == null) {
+        throw new Error(
+          "PropertyMeta range target must have an id in its meta",
+        );
+      }
+      this.range = id;
+    }
+  }
+}
