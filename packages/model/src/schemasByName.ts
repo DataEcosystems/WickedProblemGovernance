6ece9693c85b94ee125a503dcdb64a5356a1b9c8
Minor Gordon <minorg@server.fake>
2026-04-29 18:17:33 -0400

    script to generate schema indices

---
+++ packages/model/src/schemasByName.ts
@@ -0,0 +1,31 @@
+import { Architecture } from "./Architecture.js";
+import { Domain } from "./Domain.js";
+import { Ecosystem } from "./Ecosystem.js";
+import { GovernanceArtifact } from "./GovernanceArtifact.js";
+import { GovernanceArtifactType } from "./GovernanceArtifactType.js";
+import { GovernanceEpisode } from "./GovernanceEpisode.js";
+import { GovernanceEpisodeType } from "./GovernanceEpisodeType.js";
+import { GovernanceEvent } from "./GovernanceEvent.js";
+import { GovernanceEventType } from "./GovernanceEventType.js";
+import { InstitutionalLayer } from "./InstitutionalLayer.js";
+import { Organization } from "./Organization.js";
+import { Project } from "./Project.js";
+import { ProjectPartner } from "./ProjectPartner.js";
+import { ProjectPartnerRole } from "./ProjectPartnerRole.js";
+
+export const schemasByName = {
+  Architecture,
+  Domain,
+  Ecosystem,
+  GovernanceArtifact,
+  GovernanceArtifactType,
+  GovernanceEpisode,
+  GovernanceEpisodeType,
+  GovernanceEvent,
+  GovernanceEventType,
+  InstitutionalLayer,
+  Organization,
+  Project,
+  ProjectPartner,
+  ProjectPartnerRole,
+};
