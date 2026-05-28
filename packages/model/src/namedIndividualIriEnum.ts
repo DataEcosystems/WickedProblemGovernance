bf9e9113d32e09460b601788c400cc5ed8388b82
Minor Gordon <minorg@server.fake>
2026-05-28 11:30:59 -0400

    make namedIndividualIriEnum reuse iriEnum

--- packages/model/src/namedIndividualIriEnum.ts
+++ packages/model/src/namedIndividualIriEnum.ts
@@ -1,4 +1,4 @@
-import { z } from "zod";
+import { iriEnum } from "./iriEnum.js";
 import { NamedIndividualIri } from "./NamedIndividualIri.js";
 import { namedIndividuals } from "./namedIndividuals.js";
 import { WPG_CBOX } from "./namespaces.js";
@@ -9,5 +9,5 @@ export function namedIndividualIriEnum<
   const keys = Object.keys(namedIndividuals[type]).map(
     (k) => `${WPG_CBOX}${k}${type}` as NamedIndividualIri<TypeT>,
   ) as [NamedIndividualIri<TypeT>, ...NamedIndividualIri<TypeT>[]];
-  return z.enum(keys);
+  return iriEnum(keys);
 }
